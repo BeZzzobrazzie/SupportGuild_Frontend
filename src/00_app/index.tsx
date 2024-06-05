f37a6451921e592c3cb7c45236ed958a77e5a756
Zzz <n-klim29@mail.ru>
2024-06-05 10:02:37 +0300

    contextMenu add style

--- src/00_app/index.tsx
+++ src/00_app/index.tsx
@@ -1,24 +1,27 @@
-import { useDispatch } from "react-redux"
-import { ContextMenu } from "src/02_widgets/context-menu"
+import { useDispatch } from "react-redux";
+import { ContextMenu } from "src/02_widgets/context-menu";
 import { contextMenuModel } from "src/04_entities/contextmenu";
 
-
 function App() {
-
-
   const dispatch = useDispatch();
-  function contextMenuHandler() {
-    dispatch(contextMenuModel.showContextMenu())
+  function contextMenuHandler(
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) {
+    dispatch(contextMenuModel.showContextMenu());
+    event?.preventDefault();
   }
 
   return (
     <>
-      <div className="explorer" onContextMenu={contextMenuHandler}>
+      <div
+        className="explorer"
+        onContextMenu={(event) => contextMenuHandler(event)}
+      >
         explorer
       </div>
       <ContextMenu />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/02_widgets/context-menu/ui/index.tsx
+++ src/02_widgets/context-menu/ui/index.tsx
@@ -1,12 +1,27 @@
 import { useSelector } from "react-redux";
 import { RootState } from "src/00_app/store";
+import classes from "./classes.module.css";
+
 
 export function ContextMenu() {
   function showContextMenu() {}
 
-  const isShowed = useSelector((state: RootState) => state.contextMenu.isShowed);
+  const isShowed = useSelector(
+    (state: RootState) => state.contextMenu.isShowed
+  );
+  const position = {x: 0, y: 0};
 
   return (
-    <>{isShowed ? <div className="contextmenu">contextmenu</div> : <></>}</>
+    <>
+      {isShowed ? (
+        <div className={classes["context-menu"]} style={{top: position.y, left: position.x}}>
+          <button type="button">Copy</button>
+          <button type="button">Past</button>
+          <button type="button">Cut</button>
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
