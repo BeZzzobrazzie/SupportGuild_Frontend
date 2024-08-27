7fd1fe10c5b156e858b6d61d87cbb12d410aaccd
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2024-08-27 21:17:55 +0300

    +

--- src/02_widgets/output-editor/ui/index.tsx
+++ src/02_widgets/output-editor/ui/index.tsx
@@ -2,17 +2,34 @@ import { RichTextEditor } from "@mantine/tiptap";
 import Link from "@tiptap/extension-link";
 import { useEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
+import { useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from 'slate-react'
 
 export function OutputEditor() {
-  const content = "";
-  const editor = useEditor({
-    extensions: [StarterKit, Link],
-    content,
-  });
+  // const content = "";
+  // const editor = useEditor({
+  //   extensions: [StarterKit, Link],
+  //   content,
+  // });
 
+  // return (
+  //   <RichTextEditor editor={editor}>
+  //     <RichTextEditor.Content />
+  //   </RichTextEditor>
+  // );
+
+  const initialValue = [
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]
+  const [editor] = useState(() => withReact(createEditor()))
   return (
-    <RichTextEditor editor={editor}>
-      <RichTextEditor.Content />
-    </RichTextEditor>
-  );
+    // Add the editable component inside the context.
+    <Slate editor={editor} initialValue={initialValue}>
+      <Editable />
+    </Slate>
+  )
 }
